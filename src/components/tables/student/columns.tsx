0cf35fa20a05637ba5cfa0e6df1a4b71cdbdbdb5
Kyne Domerei N. Laggui <92616644+KyneLaggui@users.noreply.github.com>
2025-02-15 00:55:57 +0800

    feat: added more actions to the table

--- src/components/tables/student/columns.tsx
+++ src/components/tables/student/columns.tsx
@@ -1,6 +1,15 @@
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, MoreHorizontalIcon } from "lucide-react";
 import { TableCell } from "@/components/tables/TableCell";
 import { Badge } from "@/components/ui/badge";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns = [
   {
@@ -38,6 +47,33 @@ export const columns = [
       );
 
       return <TableCell>{totalScore}</TableCell>;
+    },
+  },
+
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const faq = row.original;
+      const [isOpen, setIsOpen] = useState(false);
+      const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+      return (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <MoreHorizontalIcon className="h-4 w-4" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>More Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                View Breakdown
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
+      );
     },
   },
 ];
