b3683efca5d5a50204a290a85d3d33e59e37dbed
Joseph Jason Buhain <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2025-02-23 23:31:13 +0800

    feat: added quiz schema validation

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -3,3 +3,11 @@ import { z } from "zod";
 export const loginSchema = z.object({
     email: z.string().email("Invalid email address").max(255)
 })
+
+export const quizSchema = z.object({
+    title: z.string().min(1, { message: "Title is required" }).max(255),
+    assessment: z.enum(["Assessment 1", "Assessment 2", "Assessment 3"], {
+        message: "Assessment must be one of the allowed values.",
+    }),
+    duration: z.coerce.number().min(1, { message: "Duration must be at least a minute." }).max(999),
+})
