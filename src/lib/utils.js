359326c20eb4ed3923bbdd34159e1ce3f4e3a4d0
Joseph Jason Buhain <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2025-02-25 23:10:16 +0800

    feat: creation of reusable toast notification

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,26 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
+import Swal from 'sweetalert2';
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
+
+export const showNotification = (type, message) => {
+  Toast.fire({
+    icon: type,
+    title: message,
+  });
+};
