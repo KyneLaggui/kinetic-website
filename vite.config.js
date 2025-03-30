4f23ac414ab27c6ade59ead9c48d7a7ba60644c1
Joseph Jason Buhain <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2025-03-30 20:18:32 +0800

    feat: added some alias pathing

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@validation": path.resolve(__dirname, "./src/lib/validation.js"),
+      "@utils": path.resolve(__dirname, "./src/lib/utils.js"),
     },
   },
 });
